56bec5e916b01ec4235dfc1f54a4af1d5cb6a2ad
Hoya517 <sv05159@naver.com>
2021-04-09 13:41:53 +0900

    index.js
    교수님 버전, 책이랑 다름

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
  * @format
  */
 
+import React from 'react'  //추가
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './app/App';  // ./App 에서 수정
 import {name as appName} from './app.json';
+const TodoApp = () => <App />  // 추가
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => App);  // 수정
