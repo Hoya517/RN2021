4aac6c3f6e2dd5f3c5d16be8252119cc183dfa94
Hoya517 <sv05159@naver.com>
2021-04-09 13:46:46 +0900

    ex 3.3
    keyboardShouldPersistTaps='always'는 키보드가 열려있으면 버튼 클릭 안됨
    flex: 1은 상위 컨테이너 영역 전체를 채움

---
+++ app/App.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import { View, ScrollView, StyleSheet } from 'react-native'
+
+class App extends Component {
+    render() {
+        return (
+            <View style={StyleSheet.container}>
+                <ScrollView keyboardShouldPersistTaps='always'
+                    style={StyleSheet.content}>
+
+                </ScrollView>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f5f5f5'
+    },
+    content: {
+        flex: 1,
+        paddingTop: 60
+    }
+})
+
+export default App;
