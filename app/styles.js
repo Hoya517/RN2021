55088fc8ed1dfc193adbb857dd91dfcc024d78ad
Hoya517 <sv05159@naver.com>
2021-04-30 15:54:16 +0900

    ex4.5
    컴포넌트 파일에서 사용하게 될 외부로 분리한 스타일

--- app/styles.js
+++ app/styles.js
@@ -1,26 +1,46 @@
-// 1. StyleSheet 임포트
 import { StyleSheet } from 'react-native'
 
-// 2. 상수에 스타일 생성
-const styles = StyleSheet.create({
+export const Colors = {
+    dark: 'black',
+    light: 'white'
+};
+
+const baseContainerStyles = {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+};
+
+const baseBoxStyles = {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 2,
+    height: 150,
+    width: 150
+};
+
+const lightStyleSheet = StyleSheet.create({
     container: {
-        marginTop: 150,
-        backgroundColor: '#ededed',
-        flexWrap: 'wrap'
+        ...baseContainerStyles,
+        backgroundColor: Colors.light
+    },
+    box: {
+        ...baseBoxStyles,
+        borderColor: Colors.dark
     }
-})
+});
 
-const buttons = StyleSheet.create({
-    primary: {
-        flex: 1,
-        height: 70,
-        backgroundColor: 'red',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginLeft: 20,
-        marginRight: 20
+const darkStyleSheet = StyleSheet.create({
+    container: {
+        ...baseContainerStyles,
+        backgroundColor: Colors.dark
+    },
+    box: {
+        ...baseContainerStyles,
+        borderColor: Colors.light
     }
-})
+});
 
-// 3. export해서 외부에서 사용할 수 있도록 함.
-export { styles, buttons }
+export default function getStyleSheet(useDarkTheme) {
+    return useDarkTheme ? darkStyleSheet : lightStyleSheet;
+}
