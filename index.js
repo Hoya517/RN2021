ecf28c2613ba00558bd2f70ee20691b75e651df4
Hoya517 <sv05159@naver.com>
2021-05-07 15:37:30 +0900

    App으로 수정

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 
 import React from 'react'  //추가
 import {AppRegistry} from 'react-native';
-import MyComponent from './app/MyComponent';  // ./MyComponent로 수정
+import App from './app/App';  // ./App으로 수정
 import {name as appName} from './app.json';
 // const TodoApp = () => <App />  // 추가
 
-AppRegistry.registerComponent(appName, () => MyComponent);  // MyComponent로 수정
+AppRegistry.registerComponent(appName, () => App);  // App으로 수정
